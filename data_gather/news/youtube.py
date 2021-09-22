48a40d23f68e8cf56ba8fbbdc364b1eda4974abe
jingshenSN2 <512320254@qq.com>
2021-09-22 17:37:36 -0400

    feat: youtube video info

--- data_gather/news/youtube.py
+++ data_gather/news/youtube.py
@@ -24,25 +24,21 @@ def get_youtube_comment(video_id):
         next_token = _get_youtube_comment(f'comments/{video_id}/{page}.json', video_id, next_token)
 
 
-get_youtube_comment('lEwZTNQCxYQ')
-get_youtube_comment('0vz-pTclTiI')
-get_youtube_comment('qlKubWocKkg')
-get_youtube_comment('zF2KF-SVIXc')
-get_youtube_comment('u1Qn_KHM1GY')
-get_youtube_comment('3LfjmI1krJs')
-get_youtube_comment('718PVQ3Oty4')
-get_youtube_comment('nl1tZx9knzA')
-get_youtube_comment('aSlI5dcpNE4')
-get_youtube_comment('HZigkcTxRo0')
-get_youtube_comment('XOeBnGirfKc')
-get_youtube_comment('t4TbEqKmdPc')
-get_youtube_comment('YqR-klyztC0')
-get_youtube_comment('2zIKPKkgzm4')
-get_youtube_comment('l1B-xsSeoCM')
-get_youtube_comment('QUv6h0eFCjg')
-get_youtube_comment('BjRcNFgNo-g')
-get_youtube_comment('u2UGqLvq0_k')
-get_youtube_comment('TI_-1dZJrk4')
-get_youtube_comment('hnOmghy5B7Y')
-get_youtube_comment('egop1jwc_Xs')
-get_youtube_comment('pZo0r45xxC0')
+def get_youtube_info(vid_list, file_name):
+    vids = ','.join(vid_list)
+    print(f'Request videos: {vids}')
+    url = f'https://www.googleapis.com/youtube/v3/videos?key={youtube_access_key}&part=snippet&id={vids}'
+    resp = requests.get(url)
+    with open(f'comments/{file_name}.json', 'wb') as f:
+        f.write(resp.content)
+    print(f'Save to comments/{file_name}')
+
+
+video_list = ['lEwZTNQCxYQ', '0vz-pTclTiI', 'qlKubWocKkg', 'zF2KF-SVIXc', 'u1Qn_KHM1GY', '3LfjmI1krJs', '718PVQ3Oty4',
+              'nl1tZx9knzA', 'aSlI5dcpNE4', 'HZigkcTxRo0', 'XOeBnGirfKc', 't4TbEqKmdPc', 'YqR-klyztC0', '2zIKPKkgzm4',
+              'l1B-xsSeoCM', 'QUv6h0eFCjg', 'BjRcNFgNo-g', 'u2UGqLvq0_k', 'TI_-1dZJrk4', 'hnOmghy5B7Y', 'egop1jwc_Xs',
+              'pZo0r45xxC0']
+
+get_youtube_info(video_list, 'video_info')
+# for vid in video_list:
+#     get_youtube_comment(vid)
