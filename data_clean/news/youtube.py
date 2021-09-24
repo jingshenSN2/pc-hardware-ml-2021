8f62f060931a5174d58ab8bc9bb36f4a8fc4b780
jingshenSN2 <512320254@qq.com>
2021-09-24 00:56:54 -0400

    feat: all videos in channel cleaned by Python

--- data_clean/news/youtube.py
+++ data_clean/news/youtube.py
@@ -1,17 +1,17 @@
 import json
 
 import pandas as pd
-
-with open('../../data_gather/news/comments/video_info.json', 'rb') as f:
-    video_info_json = json.load(f)['items']
-
 video_list = []
 
-for video in video_info_json:
-    video_info = {'video_id': video['id'], 'title': video['snippet']['title'], 'published_at': video['snippet']['publishedAt'],
-                  'description': video['snippet']['description'], 'tags': ','.join(video['snippet']['tags'])}
-    video_list.append(video_info)
+for i in range(1, 11):
+    with open(f'../../data_gather/news/comments/channel_videos_info-{i}.json', 'rb') as f:
+        video_info_json = json.load(f)['items']
+    for video in video_info_json:
+        video_info = {'video_id': video['id'], 'title': video['snippet']['title'],
+                      'published_at': video['snippet']['publishedAt'],
+                      'description': video['snippet']['description'], 'tags': ','.join(video['snippet']['tags'])}
+        video_list.append(video_info)
+
 
 video_df = pd.DataFrame(video_list)
-
-print(video_info_json)
+video_df.to_csv('../../data/channel_videos.csv', index=False)
