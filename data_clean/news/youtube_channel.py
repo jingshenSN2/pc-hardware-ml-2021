6c8337d6f17a024f0b882f532ec94524420ef507
jingshenSN2 <512320254@qq.com>
2021-09-24 17:24:51 -0400

    fix: change publishedAt to date without HH:MM:SS

--- data_clean/news/youtube_channel.py
+++ data_clean/news/youtube_channel.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 import pandas as pd
@@ -7,9 +8,11 @@ for i in range(1, 11):
     with open(f'../../data_gather/news/comments/channel_videos_info-{i}.json', 'rb') as f:
         video_info_json = json.load(f)['items']
     for video in video_info_json:
-        video_info = {'video_id': video['id'], 'title': video['snippet']['title'],
-                      'published_at': video['snippet']['publishedAt'],
-                      'description': video['snippet']['description'], 'tags': ','.join(video['snippet']['tags'])}
+        snippet = video['snippet']
+        published_at = datetime.datetime.strptime(snippet['publishedAt'], "%Y-%m-%dT%H:%M:%SZ").date()
+        video_info = {'video_id': video['id'], 'title': snippet['title'],
+                      'published_at': published_at,
+                      'description': snippet['description'], 'tags': ','.join(snippet['tags'])}
         video_list.append(video_info)
 
 
