0b59c21b86d473733b5857e9caf5f40bec7e556b
jingshenSN2 <512320254@qq.com>
2021-09-27 13:43:14 -0400

    refactor: change ggplot theme

--- eda/crypto/chia.r
+++ eda/crypto/chia.r
@@ -2,7 +2,6 @@ library(ggplot2)
 library(ggthemes)
 # devtools::install_github('cttobin/ggthemr')
 library(ggthemr)
-ggthemr('fresh')
 Sys.setlocale("LC_TIME", "English")
 setwd("D:/PycharmProjects/GU-ANLY-501-FALL-2021/data")
 
@@ -10,16 +9,16 @@ chia = read.csv("chia.csv")
 chia$Date = as.Date(chia$Date)
 p = ggplot(data=chia, aes(x=Date, y=chia.price))
 p + geom_line() +
-  theme_calc() +
-  labs(x = "Year 2021", y = 'Price(USD)', title = "Historical Price of Chia Network")
+  theme_light() +
+  labs(y = 'Price(USD)', title = "Historical Price of Chia Network")
   
 ggsave("../eda/crypto/chia_price.png")
 
 chia$market.cap = chia$market.cap / 1e6
 p = ggplot(data=chia, aes(x=Date, y=market.cap))
 p + geom_line() +
-  theme_calc() +
-  labs(x = "Year 2021", y = 'Market Capacity(million USD)', title = "Historical Market Capacity of Chia Network")
+  theme_light() +
+  labs(y = 'Market Capacity(million USD)', title = "Historical Market Capacity of Chia Network")
 
 ggsave("../eda/crypto/chia_makcap.png")
 
@@ -27,7 +26,7 @@ chia$netspace = chia$netspace / 1024**6
 
 p = ggplot(data=chia, aes(x=Date, y=netspace))
 p + geom_line() +
-  theme_calc() +
-  labs(x = "Year 2021", y = 'Net Space(Exabyte, EiB)', title = "Historical Net Space of Chia Network")
+  theme_light() +
+  labs(y = 'Net Space(Exabyte, EiB)', title = "Historical Net Space of Chia Network")
 
 ggsave("../eda/crypto/chia_netspace.png")
